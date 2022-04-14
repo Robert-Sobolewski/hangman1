604562d582d46c60e0a9739edbcf288ef09e5d63
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-04-14 18:02:32 +0200

    fixed deploy 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 import "./App.scss";
 import HomeScreen from "./screens/HomeScreen";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  HashRouter,
+} from "react-router-dom";
 import NewScreen from "./screens/NewScreen";
 import GameScreen from "./screens/GameScreen";
 import WinScreen from "./screens/WinScreen";
@@ -11,15 +16,13 @@ import LoseScreen from "./screens/LoseScreen";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomeScreen} />
-          <Route exact path="/new" component={NewScreen} />
-          <Route exact path="/game/:category" component={GameScreen} />
-          <Route exact path="/win/:wrong" component={WinScreen} />
-          <Route exact path="/lose" component={LoseScreen} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/new" component={NewScreen} />
+        <Route exact path="/game/:category" component={GameScreen} />
+        <Route exact path="/win/:wrong" component={WinScreen} />
+        <Route exact path="/lose" component={LoseScreen} />
+      </Switch>
     </div>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 require("./HomeScreen.scss");
 
 function HomeScreen() {
   return (
     <div className="home-screen">
       <h1>Hangman Game</h1>
-      <a className="button" href="/new">
+      <Link to="/new" className="button">
         new game
-      </a>
+      </Link>
     </div>
   );
 }
